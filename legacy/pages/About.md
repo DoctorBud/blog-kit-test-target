4c34bb301c10ffe914081219f34a19a3170fa89a
DoctorBud <dan@quantumclay.com>
2020-04-16 06:51:19 -0700

    Initial commit

---
+++ legacy/pages/About.md
@@ -0,0 +1,43 @@
+#### Smartdown Demos and blog-kit info
+
+This is a [GitHub Pages](https://pages.github.com)-based blog that utilizes the Jekyll services provided by GitHub, and optionally enables some posts and pages to utilize Smartdown, an in-browser renderer of enhanced Markdown and a reactive runtime.
+
+The GitHub source for this example blog is at: [smartdown/blog-kit](https://github.com/smartdown/blog-kit/)
+
+- [About smartdown/blog-kit](/pages/AboutBlogKit)
+- [Graphviz Example](/pages/Graphviz)
+- [P5JS Ellipse Example](/pages/Ellipse)
+- [P5JS Mobius Strip Example](/pages/Mobius)
+- [P5JS Dungeon Example](/pages/Dungeon)
+
+### About My Site
+
+This is my website and blog where I write about the things that interest me, including (*these are just examples*):
+
+- Beat Poetry
+- Pre-punk rock music
+- Victorian-era Science Fiction
+
+### About Me
+
+I am a nondescript being of unknown gender and background.
+
+It is probably a good idea to replace this content with something more appropriate for you, the actual author.
+
+
+#### A picture of me, the Author
+
+Smartdown (and Markdown) make it easy to include images. If you happen to have an image URL, you can use it pretty easily. For example, here's a picture of me if I happened to be a Walrus:
+
+
+![](https://upload.wikimedia.org/wikipedia/commons/thumb/c/ce/Noaa-walrus22.jpg/320px-Noaa-walrus22.jpg)
+
+
+#### Links to external resources
+
+- Wikimedia source of the above [Walrus](https://commons.wikimedia.org/wiki/File:Noaa-walrus22.jpg)
+
+
+---
+
+*Page Source: [`/pages/About.md`](/pages/About.md#-blank)*
---
+++ mm/assets/js/lunr/lunr-store.js
@@ -0,0 +1,157 @@
+var store = [{
+        "title": "Page Not Found",
+        "excerpt":"Sorry, but the page you were trying to view does not exist --- perhaps you can try searching for it below.    ","categories": [],
+        "tags": [],
+        "url": "/blog-kit-test-target/mm/404.html",
+        "teaser": null
+      },{
+        "title": "About the Site",
+        "excerpt":"#### Smartdown Demos and blog-kit info This is a [GitHub Pages](https://pages.github.com)-based blog that utilizes the Jekyll services provided by GitHub, and optionally enables some posts and pages to utilize Smartdown, an in-browser renderer of enhanced Markdown and a reactive runtime. The GitHub source for this example blog is at: [smartdown/blog-kit](https://github.com/smartdown/blog-kit/)...","categories": [],
+        "tags": [],
+        "url": "/blog-kit-test-target/mm/pages/About/",
+        "teaser": null
+      },{
+        "title": "About blog-kit",
+        "excerpt":"### About smartdown/blog-kit This is a [GitHub Pages](https://pages.github.com)-based blog that utilizes the Jekyll services provided by GitHub, and optionally enables some posts and pages to utilize Smartdown, an in-browser renderer of enhanced Markdown and a reactive runtime. - [About smartdown/blog-kit](/pages/AboutBlogKit) - [Graphviz Example](/pages/Graphviz) - [P5JS Ellipse Example](/pages/Ellipse) - [P5JS Mobius...","categories": [],
+        "tags": [],
+        "url": "/blog-kit-test-target/mm/pages/AboutBlogKit/",
+        "teaser": null
+      },{
+        "title": "Dungeon Example",
+        "excerpt":"[Character Name](:?N) ```p5js/playable/autoplay var spriteX; var spriteY; var statusHeight = 30; var controlHeight = 50; var trapezoidWidth = 100; var trapezoidHeight = 200; var upButton, downButton, leftButton, rightButton, spaceButton; p5.setup = function () { p5.createCanvas(100, 100); p5.textSize(20); p5.textFont('Helvetica'); p5.strokeWeight(2); function makeButton(label, right, bottom, keyCode) { var result = p5.createButton(label); result.class('btn-mobile-button');...","categories": [],
+        "tags": [],
+        "url": "/blog-kit-test-target/mm/pages/Dungeon/",
+        "teaser": null
+      },{
+        "title": "Ellipse",
+        "excerpt":"### About the Ellipse   - [About smartdown/blog-kit](/pages/AboutBlogKit) - [Graphviz Example](/pages/Graphviz) - [P5JS Mobius Strip Example](/pages/Mobius) - [P5JS Dungeon Example](/pages/Dungeon) - [About this Site](/pages/About)  ---  *Page Source: [`/pages/Ellipse.md`](/pages/Ellipse.md#-blank)* ","categories": [],
+        "tags": [],
+        "url": "/blog-kit-test-target/mm/pages/Ellipse/",
+        "teaser": null
+      },{
+        "title": "Graphviz Example",
+        "excerpt":"### Graphviz via `viz.js` Smartdown has integrated the powerful [viz.js](https://github.com/mdaines/viz.js) library, which is compiled into Javascript from the original Graphviz C code. Note: There is a similarly named package at [https://visjs.org](https://visjs.org) which Smartdown does not currently use. There is a Graphviz example displaying the `stdlib` namespaces at [Stdlib via Graphviz](:@Stdlib)...","categories": [],
+        "tags": [],
+        "url": "/blog-kit-test-target/mm/pages/Graphviz/",
+        "teaser": null
+      },{
+        "title": "Mobius",
+        "excerpt":"### About the Mobius  - [About smartdown/blog-kit](/pages/AboutBlogKit) - [Graphviz Example](/pages/Graphviz) - [P5JS Ellipse Example](/pages/Ellipse) - [P5JS Dungeon Example](/pages/Dungeon) - [About this Site](/pages/About)   ---  *Page Source: [`/pages/Mobius.md`](/pages/Mobius.md#-blank)*  ","categories": [],
+        "tags": [],
+        "url": "/blog-kit-test-target/mm/pages/Mobius/",
+        "teaser": null
+      },{
+        "title": "Posts by Category",
+        "excerpt":"","categories": [],
+        "tags": [],
+        "url": "/blog-kit-test-target/mm/categories/",
+        "teaser": null
+      },{
+        "title": "Posts by Tag",
+        "excerpt":"","categories": [],
+        "tags": [],
+        "url": "/blog-kit-test-target/mm/tags/",
+        "teaser": null
+      },{
+        "title": "Posts by Year",
+        "excerpt":"","categories": [],
+        "tags": [],
+        "url": "/blog-kit-test-target/mm/posts/",
+        "teaser": null
+      },{
+        "title": "Welcome to My Smartdown-enabled Blog",
+        "excerpt":"## Welcome to My Smartdown-enabled Blog This is the first post in this example Blog, created with [smartdown/blog-kit](https://github.com/smartdown/blog-kit/). ### Testing Liquid-immunity via `raw` - Test of `{{ site.baseurl }}`: {{ site.baseurl }} - Test of `{{ page.path }}`: {{ page.path }} ### Tunnel Links [Smartdown Tour](:@/posts/ABriefTourOfSmartdown.md) [Welcome To My Blog](:@/posts/WelcomeToMyBlog.md)...","categories": ["smartdown"],
+        "tags": ["welcome","smartdown"],
+        "url": "/blog-kit-test-target/mm/posts/WelcomeToMyBlog/",
+        "teaser": null
+      },{
+        "title": "Plain Old Markdown",
+        "excerpt":"## GitHub-Flavored Markdown with Kramdown BlogKit supports the standard [GitHub-flavored Markdown](https://help.github.com/articles/about-writing-and-formatting-on-github/) provided by GitHub Pages. Specifically, GitHub Pages uses the [kramdown](https://kramdown.gettalong.org/quickref.html) processor to convert Markdown into HTML within the GitHub Pages server. ### Markdown Examples Here are some simple examples of Markdown. #### Styles It's very easy to make some...","categories": ["markdown"],
+        "tags": ["markdown"],
+        "url": "/blog-kit-test-target/mm/posts/PlainOldMarkdown/",
+        "teaser": null
+      },{
+        "title": "A Brief Tour of Smartdown",
+        "excerpt":"# WelcomeToSmartdown --- ## Welcome to Smartdown Smartdown is an extension of Markdown that includes several additions: - Technical notation support, including [MathJax](https://www.mathjax.org) LaTeX and AsciiMath syntax, chemical notation via [`mhchem`](https://mhchem.github.io/MathJax-mhchem/), - Syntax highlighting via [highlight.js](https://highlightjs.org). - Reactive cells similar to those in a spreadsheet, but inline with Smartdown prose....","categories": ["smartdown"],
+        "tags": ["welcome","smartdown"],
+        "url": "/blog-kit-test-target/mm/posts/ABriefTourOfSmartdown/",
+        "teaser": null
+      },{
+        "title": "Page Not Found",
+        "excerpt":"Sorry, but the page you were trying to view does not exist --- perhaps you can try searching for it below.    ","categories": [],
+        "tags": [],
+        "url": "/blog-kit-test-target/mm/404.html",
+        "teaser": null
+      },{
+        "title": "About the Site",
+        "excerpt":"#### Smartdown Demos and blog-kit info This is a [GitHub Pages](https://pages.github.com)-based blog that utilizes the Jekyll services provided by GitHub, and optionally enables some posts and pages to utilize Smartdown, an in-browser renderer of enhanced Markdown and a reactive runtime. The GitHub source for this example blog is at: [smartdown/blog-kit](https://github.com/smartdown/blog-kit/)...","categories": [],
+        "tags": [],
+        "url": "/blog-kit-test-target/mm/pages/About",
+        "teaser": null
+      },{
+        "title": "About blog-kit",
+        "excerpt":"### About smartdown/blog-kit This is a [GitHub Pages](https://pages.github.com)-based blog that utilizes the Jekyll services provided by GitHub, and optionally enables some posts and pages to utilize Smartdown, an in-browser renderer of enhanced Markdown and a reactive runtime. - [About smartdown/blog-kit](/pages/AboutBlogKit) - [Graphviz Example](/pages/Graphviz) - [P5JS Ellipse Example](/pages/Ellipse) - [P5JS Mobius...","categories": [],
+        "tags": [],
+        "url": "/blog-kit-test-target/mm/pages/AboutBlogKit",
+        "teaser": null
+      },{
+        "title": "Dungeon Example",
+        "excerpt":"[Character Name](:?N) ```p5js/playable/autoplay var spriteX; var spriteY; var statusHeight = 30; var controlHeight = 50; var trapezoidWidth = 100; var trapezoidHeight = 200; var upButton, downButton, leftButton, rightButton, spaceButton; p5.setup = function () { p5.createCanvas(100, 100); p5.textSize(20); p5.textFont('Helvetica'); p5.strokeWeight(2); function makeButton(label, right, bottom, keyCode) { var result = p5.createButton(label); result.class('btn-mobile-button');...","categories": [],
+        "tags": [],
+        "url": "/blog-kit-test-target/mm/pages/Dungeon",
+        "teaser": null
+      },{
+        "title": "Ellipse",
+        "excerpt":"### About the Ellipse   - [About smartdown/blog-kit](/pages/AboutBlogKit) - [Graphviz Example](/pages/Graphviz) - [P5JS Mobius Strip Example](/pages/Mobius) - [P5JS Dungeon Example](/pages/Dungeon) - [About this Site](/pages/About)  ---  *Page Source: [`/pages/Ellipse.md`](/pages/Ellipse.md#-blank)* ","categories": [],
+        "tags": [],
+        "url": "/blog-kit-test-target/mm/pages/Ellipse",
+        "teaser": null
+      },{
+        "title": "Graphviz Example",
+        "excerpt":"### Graphviz via `viz.js` Smartdown has integrated the powerful [viz.js](https://github.com/mdaines/viz.js) library, which is compiled into Javascript from the original Graphviz C code. Note: There is a similarly named package at [https://visjs.org](https://visjs.org) which Smartdown does not currently use. There is a Graphviz example displaying the `stdlib` namespaces at [Stdlib via Graphviz](:@Stdlib)...","categories": [],
+        "tags": [],
+        "url": "/blog-kit-test-target/mm/pages/Graphviz",
+        "teaser": null
+      },{
+        "title": "Mobius",
+        "excerpt":"### About the Mobius  - [About smartdown/blog-kit](/pages/AboutBlogKit) - [Graphviz Example](/pages/Graphviz) - [P5JS Ellipse Example](/pages/Ellipse) - [P5JS Dungeon Example](/pages/Dungeon) - [About this Site](/pages/About)   ---  *Page Source: [`/pages/Mobius.md`](/pages/Mobius.md#-blank)*  ","categories": [],
+        "tags": [],
+        "url": "/blog-kit-test-target/mm/pages/Mobius",
+        "teaser": null
+      },{
+        "title": "Posts by Category",
+        "excerpt":"","categories": [],
+        "tags": [],
+        "url": "/blog-kit-test-target/mm/categories/",
+        "teaser": null
+      },{
+        "title": "Posts by Tag",
+        "excerpt":"","categories": [],
+        "tags": [],
+        "url": "/blog-kit-test-target/mm/tags/",
+        "teaser": null
+      },{
+        "title": "Posts by Year",
+        "excerpt":"","categories": [],
+        "tags": [],
+        "url": "/blog-kit-test-target/mm/posts/",
+        "teaser": null
+      },{
+        "title": "Welcome to My Smartdown-enabled Blog",
+        "excerpt":"## Welcome to My Smartdown-enabled Blog This is the first post in this example Blog, created with [smartdown/blog-kit](https://github.com/smartdown/blog-kit/). ### Testing Liquid-immunity via `raw` - Test of `{{ site.baseurl }}`: {{ site.baseurl }} - Test of `{{ page.path }}`: {{ page.path }} ### Tunnel Links [Smartdown Tour](:@/posts/ABriefTourOfSmartdown.md) [Welcome To My Blog](:@/posts/WelcomeToMyBlog.md)...","categories": ["smartdown"],
+        "tags": ["welcome","smartdown"],
+        "url": "/blog-kit-test-target/mm/posts/WelcomeToMyBlog",
+        "teaser": null
+      },{
+        "title": "Plain Old Markdown",
+        "excerpt":"## GitHub-Flavored Markdown with Kramdown BlogKit supports the standard [GitHub-flavored Markdown](https://help.github.com/articles/about-writing-and-formatting-on-github/) provided by GitHub Pages. Specifically, GitHub Pages uses the [kramdown](https://kramdown.gettalong.org/quickref.html) processor to convert Markdown into HTML within the GitHub Pages server. ### Markdown Examples Here are some simple examples of Markdown. #### Styles It's very easy to make some...","categories": ["markdown"],
+        "tags": ["markdown"],
+        "url": "/blog-kit-test-target/mm/posts/PlainOldMarkdown",
+        "teaser": null
+      },{
+        "title": "A Brief Tour of Smartdown",
+        "excerpt":"# WelcomeToSmartdown --- ## Welcome to Smartdown Smartdown is an extension of Markdown that includes several additions: - Technical notation support, including [MathJax](https://www.mathjax.org) LaTeX and AsciiMath syntax, chemical notation via [`mhchem`](https://mhchem.github.io/MathJax-mhchem/), - Syntax highlighting via [highlight.js](https://highlightjs.org). - Reactive cells similar to those in a spreadsheet, but inline with Smartdown prose....","categories": ["smartdown"],
+        "tags": ["welcome","smartdown"],
+        "url": "/blog-kit-test-target/mm/posts/ABriefTourOfSmartdown",
+        "teaser": null
+      }]
